c2a4235c49485fec073590fbc60546b046d55e79
Bianca Souto <bianca.gomes3021@gmail.com>
2024-09-17 09:52:36 -0300

    Primeiro commit

---
+++ ScreenSoundP2/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+namespace ScreenSoundP2.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as suas músicas favoritas: '{Nome}'");
+        foreach (var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+        Console.WriteLine();
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new     //Vamos serializar, vamos fazer o oposto, ou seja, convertelos em arquivos Json
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas
+        });
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo Json foi criado com sucesso.{Path.GetFullPath(nomeDoArquivo)}");
+    }
+}
---
+++ ScreenSoundP2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using ScreenSoundP2.Modelos;
+using ScreenSoundP2.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;  //Json convertido para um objeto manipulável no C#
+        musicas[563].ExibirDetahesDaMusica();
+
+        //LinqFilter.FiltrarTodasAsMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Post Malone");
+        LinqFilter.FiltrarMusicarPeloAno(musicas, 2015);
+        //LinqFilter.FiltrarMusicasPelaTonalidade(musicas, "C#");
+
+
+        var musicasPreferidas1 = new MusicasPreferidas("Musicas 1");
+        musicasPreferidas1.AdicionarMusicasFavoritas(musicas[567]);
+        musicasPreferidas1.AdicionarMusicasFavoritas(musicas[777]);
+        musicasPreferidas1.AdicionarMusicasFavoritas(musicas[48]);
+        musicasPreferidas1.AdicionarMusicasFavoritas(musicas[695]);
+        musicasPreferidas1.AdicionarMusicasFavoritas(musicas[147]);
+        musicasPreferidas1.ExibirMusicasFavoritas();
+
+        var musicasPreferidas2 = new MusicasPreferidas("Musicas 2");
+        musicasPreferidas2.AdicionarMusicasFavoritas(musicas[13]);
+        musicasPreferidas2.AdicionarMusicasFavoritas(musicas[445]);
+        musicasPreferidas2.AdicionarMusicasFavoritas(musicas[25]);
+        musicasPreferidas2.ExibirMusicasFavoritas();
+
+        musicasPreferidas1.GerarArquivoJson();
+    }
+    catch (Exception ex) 
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
